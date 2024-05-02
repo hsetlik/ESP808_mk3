3fe1aa30339b5358c4e47ed996d1643a9407ae75
Hayden Setlik <haydensetlik@gmail.com>
2024-05-02 13:20:33 -0600

    Sample playback stuff

--- include/Sample.h
+++ include/Sample.h
@@ -1,7 +1,21 @@
 #pragma once
 #include "Audio.h"
 
+// defaults and ranges for the digital voices' parameters
 
+#define PAN_MIN 0.0f
+#define PAN_MAX 1.0f
+#define PAN_DEFAULT 0.5f
+
+#define RETRIG_LENGTH 600
+
+
+enum SampleState : uint8_t
+{
+Idle,
+Playing,
+Retrig
+};
 
 
 class Sample
@@ -9,14 +23,26 @@ class Sample
     private:
         const String fileName;
         // playback state stuff
-        bool isPlaying;
+        SampleState state;
         unsigned long sampleIdx;
         // the buffer to be allocated in PSRAM
         float* data;
         unsigned long lengthSamples;
 
+        // pan and level state
+        float pan;
+        float currentLevel;
+        
+        float trackLevel;
+
+        // retrigger handling stuff
+        uint16_t samplesIntoRetrig;
+
     public:
         Sample(const String& path);
         ~Sample();
+        // trigger this sample
+        void trigger(uint8_t level=127);
 
+        void renderNext(float* l, float* r);
 };
--- src/Sample.cpp
+++ src/Sample.cpp
@@ -3,10 +3,14 @@
 
 Sample::Sample(const String& name) : 
 fileName(name),
-isPlaying(false),
+state(SampleState::Idle),
 sampleIdx(0),
 data(nullptr),
-lengthSamples(0)
+lengthSamples(0),
+pan(PAN_DEFAULT),
+currentLevel(1.0f),
+trackLevel(1.0f),
+samplesIntoRetrig(0)
 {
     if(!SD.exists(fileName))
     {
@@ -32,4 +36,58 @@ lengthSamples(0)
 Sample::~Sample()
 {
     free(data);
+}
+
+void Sample::trigger(uint8_t level)
+{
+    currentLevel = trackLevel * ((float)level / 127.0f);
+    if(state == SampleState::Idle)
+    {
+        state = SampleState::Playing;
+        sampleIdx = 0;
+        samplesIntoRetrig = 0;
+    }
+    else if (lengthSamples - sampleIdx > RETRIG_LENGTH)
+    {
+        state == SampleState::Retrig;
+        samplesIntoRetrig = 0;
+    }
+}
+
+void Sample::renderNext(float* l, float* r)
+{
+    switch(state)
+    {
+     case Idle:
+        break;
+    case Playing:
+        float& fMono = data[sampleIdx];
+        *l += currentLevel * (fMono * pan);
+        *r += currentLevel * (fMono * (1.0f - pan));
+        ++sampleIdx;
+        if(sampleIdx == lengthSamples) // we've finished playback
+        {
+            state = SampleState::Idle;
+            sampleIdx = 0;
+        }
+        break;
+    case Retrig:
+        float fMix = (float)samplesIntoRetrig / (float)RETRIG_LENGTH;
+        float fMono = (data[samplesIntoRetrig] * fMix) + (data[sampleIdx] * (1.0f - fMix));
+        *l += currentLevel * (fMono * pan);
+        *r += currentLevel * (fMono * (1.0f - pan));
+        ++samplesIntoRetrig;
+        ++sampleIdx;
+        // if we're at the end of the retrig
+        if(samplesIntoRetrig == RETRIG_LENGTH)
+        {
+            sampleIdx = samplesIntoRetrig;
+            samplesIntoRetrig = 0;
+            state = SampleState::Playing;
+        }
+        break;
+    default:
+        break;   
+    }
+
 }
